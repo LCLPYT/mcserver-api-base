5787aa77330fc55d09dddb5a7553ce98770a4d23
LCLP <lukas@lclpnet.work>
2021-04-28 16:34:54 +0200

    split into modules

--- impl-bukkit/src/main/java/work/lclpnet/serverapi/bukkit/BukkitImpl.java
+++ impl-bukkit/src/main/java/work/lclpnet/serverapi/bukkit/BukkitImpl.java
@@ -4,7 +4,12 @@
  * Licensed under the MIT License. For more information, consider the LICENSE file in the project's root directory.
  */
 
-rootProject.name = 'MCServerAPI'
-include 'impl-bukkit'
-include 'base'
+package work.lclpnet.serverapi.bukkit;
 
+public class BukkitImpl {
+
+    public static void method() {
+        System.out.println("HELLO BUKKIT");
+    }
+
+}
